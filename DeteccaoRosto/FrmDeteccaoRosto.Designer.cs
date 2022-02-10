52583aa9eb3f30d94bec005eb0d7aa5f4a1d29e2
User <User@GTX>
2022-02-10 17:50:53 -0300

    Add project files.

---
+++ DeteccaoRosto/FrmDeteccaoRosto.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DeteccaoRosto
+{
+    partial class FrmDeteccaoRosto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirImagemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.iniciarWebcamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pararWebcamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirImagemToolStripMenuItem,
+            this.iniciarWebcamToolStripMenuItem,
+            this.pararWebcamToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(551, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirImagemToolStripMenuItem
+            // 
+            this.abrirImagemToolStripMenuItem.Name = "abrirImagemToolStripMenuItem";
+            this.abrirImagemToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.abrirImagemToolStripMenuItem.Text = "Abrir Imagem";
+            this.abrirImagemToolStripMenuItem.Click += new System.EventHandler(this.abrirImagemToolStripMenuItem_Click);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImagem.Location = new System.Drawing.Point(0, 24);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(551, 579);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagem.TabIndex = 1;
+            this.picImagem.TabStop = false;
+            // 
+            // iniciarWebcamToolStripMenuItem
+            // 
+            this.iniciarWebcamToolStripMenuItem.Name = "iniciarWebcamToolStripMenuItem";
+            this.iniciarWebcamToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.iniciarWebcamToolStripMenuItem.Text = "Iniciar Webcam";
+            this.iniciarWebcamToolStripMenuItem.Click += new System.EventHandler(this.iniciarWebcamToolStripMenuItem_Click);
+            // 
+            // pararWebcamToolStripMenuItem
+            // 
+            this.pararWebcamToolStripMenuItem.Name = "pararWebcamToolStripMenuItem";
+            this.pararWebcamToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.pararWebcamToolStripMenuItem.Text = "Parar Webcam";
+            this.pararWebcamToolStripMenuItem.Click += new System.EventHandler(this.pararWebcamToolStripMenuItem_Click);
+            // 
+            // FrmDeteccaoRosto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 603);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmDeteccaoRosto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Detecção de Rosto";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirImagemToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarWebcamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pararWebcamToolStripMenuItem;
+    }
+}
+
---
+++ DeteccaoRosto/FrmDeteccaoRosto.cs
@@ -0,0 +1,94 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeteccaoRosto
+{
+    public partial class FrmDeteccaoRosto : Form
+    {
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        VideoCapture capture;
+
+        public FrmDeteccaoRosto()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcesarImagem(Bitmap btm)
+        {
+            Bitmap bitmap = (Bitmap)btm.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.4, 1);
+            foreach (var rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            picImagem.Image = bitmap;
+        }
+private void abrirImagemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, Filter = "JPEG|*.jpg"})
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    ProcesarImagem(new Bitmap(Image.FromFile(ofd.FileName)));
+                }
+            }
+        }
+
+        private void iniciarWebcamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(capture != null)
+            {
+                MessageBox.Show("A webcam já está iniciada!");
+                return;
+            }
+            capture = new VideoCapture(0);
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+            capture.Start();
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                ProcesarImagem(m.Bitmap);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pararWebcamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                MessageBox.Show("A webcam já está desligada!");
+                return;
+            }
+
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+            capture.Stop();
+            capture.Dispose();
+            capture = null;
+            picImagem.Image = null;
+        }
+    }
+}
